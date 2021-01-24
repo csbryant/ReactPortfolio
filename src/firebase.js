afd35d01fa13f29bcee7fd866287049d7ca0e45a
Chris Bryant <67081049+csbryant@users.noreply.github.com>
2021-01-24 15:38:35 -0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 var firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyAOxotwWdwewgkfk8x6e63t3T8gCvfCqLk",
-    authDomain: "newport-671a7.firebaseapp.com",
-    projectId: "newport-671a7",
-    storageBucket: "newport-671a7.appspot.com",
-    messagingSenderId: "593470078924",
-    appId: "1:593470078924:web:846ba1e74b8b4cb50ba769"
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: process.env.REACT_APP_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_SENDER_ID,
+    appId: process.env.REACT_APP_ID
 }) 
 var db = firebaseApp.firestore();
 
